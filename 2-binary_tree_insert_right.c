80c16c2a03649aa367848bf2c040c995b4aae2df
Ige Victor <victorkoladeige@gmail.com>
2023-08-02 15:19:40 -0700

    insert right node and left node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - a function that creates a binary tree node.
+ * @parent: the parent node.
+ * @value: the node value.
+ * Return: new node.
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode, *temp;
+
+	newnode = binary_tree_node(parent, value);
+	if (!newnode || !parent)
+		return (NULL);
+	if (parent->right != NULL)
+	{
+		temp = parent->right;
+		parent->right = newnode;
+		temp->parent = newnode;
+		newnode->right = temp;
+	}
+	parent->right = newnode;
+
+	return (newnode);
+}
