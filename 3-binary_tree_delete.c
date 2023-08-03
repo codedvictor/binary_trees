90b58cc78b91a384de577f1a89a94d961aba5841
Ige Victor <victorkoladeige@gmail.com>
2023-08-03 01:56:03 -0700

    delete tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,6 +8,9 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree)
-		free(tree);
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
