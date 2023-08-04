a0fa699a32aaefc1cd6ea767bab7ad22a1ee5067
Ige Victor <victorkoladeige@gmail.com>
2023-08-03 17:08:38 -0700

    updated file

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	parent = node->parent;
 
-	if (parent->left && parent->left != node)
-		return (parent->left);
-	return (parent->right);
+	if (parent->left && parent->left == node)
+		return (parent->right);
+	return (parent->left);
 }
